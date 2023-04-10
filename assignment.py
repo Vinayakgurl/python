5ea8c7ad78bca759d9eda52d7c516336fce45b83
Vinayakgurl <128384086+Vinayakgurl@users.noreply.github.com>
2023-04-10 17:36:56 +0530

    aaaa
    aaaa

---
+++ assignment.py
@@ -0,0 +1,61 @@
+
+# finding the largest number 
+# a = input("enter the aze of a = ")
+# b = input("enter the aze of b = ")
+# c = input("enter the aze of c = ")
+
+# if(a>b and a>c) :
+#  print("the largest number is a")
+# elif(b>a and b>c):
+#  print("the largest number is b")
+# elif(c>a and c>b):
+#  print("the largest number is c")
+# elif(a==b and b==c):
+#  print("all number are equal")
+ 
+ 
+ 
+ #n finding prime number
+ 
+prime = input()
+if(prime in range(2)):
+ add = (prime[0])+(prime[1])
+ prime=add
+ 
+if(prime%3==0  or prime[len(prime)-1] not in range (0,2,4,6,8 )) :
+
+     print("enter numbered is not a prime number")
+     
+else :
+   print("entered number is prime number")
+  
+  
+# mathamatical operator
+# a = int(input("entere tyhe number"))
+# if(a>0):
+#     print("enter number is positive" )
+# else:
+#  print("enter number is negative ")
+    
+# score 
+# physics = int(input("enter the marks"))
+# chemistry = int(input("enter the marks"))
+# bio = int(input("enter the marks"))
+# maths = int(input("enter the marks"))
+# score = (physics +chemistry+maths +bio)*100/4
+
+# if(score >= 60):
+#     print("you are pass")
+# else:
+#    print ("you are fail")
+   
+#calculator 
+# a = int(input("enter the number"))
+# b = int(input("enter the another number"))
+# switch = {
+#  '+': print(a+b);
+#  '-': print(a-b);
+#  '*': print(a*b);  
+#  '/': print(a/b);
+#  '%':print(a%b) ; 
+# } 
